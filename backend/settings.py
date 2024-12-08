94baa5c35e420638839dcef0aa12a532288d6909
Robert E E <153441621+LeonByte@users.noreply.github.com>
2024-12-08 22:59:03 +0100

    update backend/settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -1,3 +1,22 @@
+STATIC_URL = '/static/'
+
+# For template loading
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'frontend'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
