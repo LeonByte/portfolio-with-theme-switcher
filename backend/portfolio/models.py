47f60e44b6599e4e3501e1dc9b0484335ef40434
Robert E E <153441621+LeonByte@users.noreply.github.com>
2024-12-11 18:56:00 +0100

    update backend/portfolio/models.py

--- backend/portfolio/models.py
+++ backend/portfolio/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    theme = models.CharField(max_length=10, default='light')  # Default theme is light
+
+    def __str__(self):
+        return self.user.username
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
